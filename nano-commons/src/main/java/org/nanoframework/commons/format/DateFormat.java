c3dc84f7420a59e79e723a9a630723573a826d34
yanghe <comicme_yanghe@icloud.com>
2016-01-12 19:16:20 +0800

    调整DateFormat类

--- nano-commons/src/main/java/org/nanoframework/commons/format/DateFormat.java
+++ nano-commons/src/main/java/org/nanoframework/commons/format/DateFormat.java
@@ -21,47 +21,60 @@ import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.nanoframework.commons.util.Assert;
+
 /**
- * 使用commons-lang3中的DateFormatUtils替代此实现
- * 
  * @author yanghe
  * @date 2015年8月19日 上午8:58:41
- * 
- * @see org.apache.commons.lang3.time.DateFormatUtils
  */
-@Deprecated
 public class DateFormat {
-
-	private static final ConcurrentMap<Pattern, ThreadLocal<SimpleDateFormat>> FORMAT_MAP = new ConcurrentHashMap<>();
+	private static final ConcurrentMap<String, ThreadLocal<SimpleDateFormat>> FORMAT_MAP = new ConcurrentHashMap<>();
 	
-	public static final SimpleDateFormat getAndSetDateFormat(Pattern pattern) {
+	public static final SimpleDateFormat get(String pattern) {
 		SimpleDateFormat format;
 		ThreadLocal<SimpleDateFormat> formatLocal;
 		if((formatLocal = FORMAT_MAP.get(pattern)) == null) {
 			formatLocal = new ThreadLocal<>();
-			formatLocal.set(format = new SimpleDateFormat(pattern.get()));
+			formatLocal.set(format = new SimpleDateFormat(pattern));
 			FORMAT_MAP.put(pattern, formatLocal);
 		}
 		
 		if((format = formatLocal.get()) == null) {
-			format = new SimpleDateFormat(pattern.get());
+			format = new SimpleDateFormat(pattern);
 			formatLocal.set(format);
 		}
 		
 		return format;
 	}
 	
+	public static final SimpleDateFormat get(Pattern pattern) {
+		Assert.notNull(pattern, "pattern must be not null.");
+		return get(pattern.get());
+	}
+	
 	public static final String format(Date date, Pattern pattern) {
-		return getAndSetDateFormat(pattern).format(date);
+		return get(pattern).format(date);
+	}
+	
+	public static final String format(Date date, String pattern) {
+		return get(pattern).format(date);
 	}
 	
 	public static final String format(Object date, Pattern pattern) {
-		return getAndSetDateFormat(pattern).format(date); 
+		return get(pattern).format(date); 
+	}
+	
+	public static final String format(Object date, String pattern) {
+		return get(pattern).format(date); 
 	}
 
 	@SuppressWarnings("unchecked")
 	public static final <T extends Date> T parse(String date, Pattern pattern) throws ParseException {
-		return (T) getAndSetDateFormat(pattern).parse(date);
+		return (T) get(pattern).parse(date);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static final <T extends Date> T parse(String date, String pattern) throws ParseException {
+		return (T) get(pattern).parse(date);
+	}
 }
