4575bd26553fa90bfe55683ad0ae8cdcf332c0e5
yanghe <comicme_yanghe@icloud.com>
2016-03-06 13:36:48 +0800

    update HttpResponse

--- nano-core/src/main/java/org/nanoframework/core/httpclient/HttpResponse.java
+++ nano-core/src/main/java/org/nanoframework/core/httpclient/HttpResponse.java
@@ -3,34 +3,34 @@ package org.nanoframework.core.httpclient;
 import org.nanoframework.commons.entity.BaseEntity;
 
 public class HttpResponse extends BaseEntity {
-		private static final long serialVersionUID = -3602195552991064086L;
-		
-		public static final HttpResponse EMPTY = create(0, "", "");
-		
-		public final int statusCode;
-		public final String reasonPhrase;
-		public final String entity;
-		
-		public HttpResponse(int statusCode, String reasonPhrase, String entity) {
-			this.statusCode = statusCode;
-			this.reasonPhrase = reasonPhrase;
-			this.entity = entity;
-		}
-		
-		public static final HttpResponse create(int statusCode, String reasonPhrase, String entity) {
-			return new HttpResponse(statusCode, reasonPhrase, entity);
-		}
+	private static final long serialVersionUID = -3602195552991064086L;
 
-		public int getStatusCode() {
-			return statusCode;
-		}
+	public static final HttpResponse EMPTY = create(0, "", "");
 
-		public String getReasonPhrase() {
-			return reasonPhrase;
-		}
+	public final int statusCode;
+	public final String reasonPhrase;
+	public final String entity;
 
-		public String getEntity() {
-			return entity;
-		}
-
+	public HttpResponse(int statusCode, String reasonPhrase, String entity) {
+		this.statusCode = statusCode;
+		this.reasonPhrase = reasonPhrase;
+		this.entity = entity;
 	}
+
+	public static final HttpResponse create(int statusCode, String reasonPhrase, String entity) {
+		return new HttpResponse(statusCode, reasonPhrase, entity);
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public String getReasonPhrase() {
+		return reasonPhrase;
+	}
+
+	public String getEntity() {
+		return entity;
+	}
+
+}
