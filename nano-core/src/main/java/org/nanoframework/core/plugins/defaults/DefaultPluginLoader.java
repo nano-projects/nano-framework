e937dae37a4b826e63bdd357d461366b2dca837e
yanghe <comicme_yanghe@icloud.com>
2016-04-12 23:40:38 +0800

    remove core httpclient and plugin

--- nano-core/src/main/java/org/nanoframework/core/plugins/defaults/DefaultPluginLoader.java
+++ nano-core/src/main/java/org/nanoframework/core/plugins/defaults/DefaultPluginLoader.java
@@ -22,7 +22,6 @@ import org.nanoframework.core.plugins.Plugin;
 import org.nanoframework.core.plugins.PluginLoader;
 import org.nanoframework.core.plugins.defaults.module.AOPModule;
 import org.nanoframework.core.plugins.defaults.module.DataSourceModule;
-import org.nanoframework.core.plugins.defaults.plugin.HttpClientPlugin;
 import org.nanoframework.core.plugins.defaults.plugin.JedisPlugin;
 import org.nanoframework.core.plugins.defaults.plugin.Log4j2Plugin;
 import org.nanoframework.core.plugins.defaults.plugin.QuartzPlugin;
@@ -55,7 +54,6 @@ public class DefaultPluginLoader extends PluginLoader {
 		plugins.add(new QuartzPlugin());
 		plugins.add(new SchedulerPlugin());
 		plugins.add(new WebSocketPlugin());
-		plugins.add(new HttpClientPlugin());
 		
 	}
 
